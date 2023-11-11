5baba83f4bf0eb7b963c45bd2ac3207741625c53
malik672 <aremumalik01@gmail.com>
2023-11-11 15:39:44 +0100

    add types

--- src/ast/types.rs
+++ src/ast/types.rs
@@ -12,17 +12,26 @@ pub struct type_descriptions {
 
 //Define custom types in huff
 pub enum type_name {
-    error_name,
-    function_type_name,
-    macro_type_name,
-    constant(storage),
-    event,
-    jump_table,
-    code_table
+    error_name(error_huff),
+    function_type_name(function_huff),
+    macro_type_name(macro_huff),
+    constant(storage_huff),
+    event(event_huff),
+    jump_table(jump_huff),
+    code_table(code_huff)
 }
 
 impl Node for type_name {
     fn accept(&self, definitions: &mut impl ast_definitions) -> Result<()> {
+       match self {
+        type_name::error_name(error_huff) => error_huff.accept(ast_definitions),
+        type_name::function_type_name(function_huff) => function_huff.accept(ast_definitions),
+        type_name::macro_type_name(macro_huff) => macro_huff.accept(ast_definitions),
+        type_name::constant(storage_huff) => storage_huff.accept(ast_definitions),
+        type_name::event(event_huff) => event_huff.accept(ast_definitions),
+        type_name::jump_table(jump_huff) => jump_huff.accept(ast_definitions),
+        type_name::code_table(code_huff) => code_huff.accept(ast_definitions)
 
+       }
     }
 }
