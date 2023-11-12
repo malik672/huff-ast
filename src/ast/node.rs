710283a1edd0f9a817ae4994f2e31ac70035196a
malik672 <aremumalik01@gmail.com>
2023-11-12 08:02:25 +0100

    implement-node

---
+++ src/ast/node.rs
@@ -0,0 +1,16 @@
+
+pub enum NodeType {
+    Block,
+    Label,
+    IdentifierPath,
+    Error,
+    StructuredDocumentation,
+    BlockDocumentaion,
+    FunctionTypeName,
+    FunctionCall,
+    Identifier,
+    Expression,
+    ParamType,
+    ParamList,
+    RevertStatement,
+}
--- src/ast/types.rs
+++ src/ast/types.rs
@@ -19,22 +19,57 @@ pub enum type_name {
     event(event_huff),
     jump_table(jump_huff),
     code_table(code_huff),
-    String(String),
+    String(string),
 }
 
 impl Node for type_name {
     fn accept(&self, definitions: &mut impl ast_definitions) -> Result<()> {
        match self {
-        type_name::error_name(error_huff) => error_huff.accept(ast_definitions),
-        type_name::function_type_name(function_huff) => function_huff.accept(ast_definitions),
-        type_name::macro_type_name(macro_huff) => macro_huff.accept(ast_definitions),
-        type_name::constant(storage_huff) => storage_huff.accept(ast_definitions),
-        type_name::event(event_huff) => event_huff.accept(ast_definitions),
-        type_name::jump_table(jump_huff) => jump_huff.accept(ast_definitions),
-        type_name::code_table(code_huff) => code_huff.accept(ast_definitions)
+        type_name::error_name(error_huff) => error_huff.accept(definitions),
+        type_name::function_type_name(function_huff) => function_huff.accept(definitions),
+        type_name::macro_type_name(macro_huff) => macro_huff.accept(definitions),
+        type_name::constant(storage_huff) => storage_huff.accept(definitions),
+        type_name::event(event_huff) => event_huff.accept(definitions),
+        type_name::jump_table(jump_huff) => jump_huff.accept(definitions),
+        type_name::code_table(code_huff) => code_huff.accept(definitions),
         type_name::String(_) => {
             panic!()
         }
        }
+    }
+}
+
+impl Display for type_name {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self{
+            type_name::error_name(error_huff) => error_huff.fmt(f),
+            type_name::function_type_name(code_huff) => code_huff.fmt(f),
+            type_name::macro_type_name(macro_huff) => macro_huff.fmt(f),
+            type_name::constant(storage_huff) => storage_huff.fmt(f),
+            type_name::event(event_huff) => event_huff.fmt(f),
+            type_name::jump_table(jump_huff) => jump_huff.fmt(f),
+            type_name::code_table(code_huff) => code_huff.fmt(f),
+            type_name::String(string) => string.fmt(f), 
+              _=> panic!("Invalid"),
+        }
+    }
+}
+
+pub struct function_type {
+    pub visibility: Option<String>,
+    pub f_type: Option<String>,
+    pub param_type: Option<Vec<String>>,
+    pub return_param: Option<Vec<String>>,
+    pub f_descriptions: Option<String>,
+    pub is_receive_ether: Option<String>
+}
+
+impl Node for function_type {
+    fn accept(&self, f_type: &mut impl ast_definitions) -> Result<()> {
+        if f_type.visit_function_type_name(self)? {
+            self.param_type.accept(f_type)?;
+            self.return_param.accept(f_type)?;
+        }
+        f_type.end_visit.function_type_name(self)
     }
 }
