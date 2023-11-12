4a21ba3e2901d032ee62103272710babcddd2a3b
malik672 <aremumalik01@gmail.com>
2023-11-12 08:51:55 +0100

    implement-node

--- src/definitions/ast_definitions.rs
+++ src/definitions/ast_definitions.rs
@@ -3,3 +3,17 @@ use ast::*;
 pub trait ast_definitions {
 
 }
+
+
+pub trait Node {
+    fn accept(&self, f_type: &mut impl ast_definitions) -> Result<()> {
+        Ok(())
+    }
+}
+
+pub fn list_accept(list: &Vec<impl Node>, f_type: &mut impl ast_definitions) -> Result<()> {
+    for elem in list {
+        elem.accept(f_type)?;
+    }
+    Ok(())
+}
